720a53916d378a2aefdfa58cfa5d07b20d6c73ec
Jean BASTIN <j.bastin@kayrros.com>
2018-03-02 14:47:18 +0000

    Update get_sentinel1.py

--- get_sentinel1.py
+++ get_sentinel1.py
@@ -107,7 +107,7 @@ def download_sentinel_image(image, out_dir='', mirror='code-de'):
                 download_sentinel_image(image, out_dir, mirror='scihub')
         elif mirror == 'scihub':
             url = "{}/odata/v1/Products('{}')/$value".format(scihub_url, image['id'])
-            query_data_hub(zip_path, url, verbose=True)
+            query_data_hub(zip_path, url, verbose=False)
         else:
             print('ERROR: unknown mirror {}'.format(mirror))
 
