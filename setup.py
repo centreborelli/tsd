ad3c17503da7f8d72d56dcdf174c6cbd97679d03
Nicolas Conti <n.conti@kayrros.com>
2018-07-26 15:11:06 +0200

    Packaging module for pip installation

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="tsd",
+    version="0.1",
+    description='Automatic download of Sentinel, Landsat and Planet crops.',
+    long_description=readme(),
+    author='Carlo de Franchis (CMLA)'
+           'With contributions from Enric Meinhardt-Llopis (ENS Cachan), Axel Davy (CMLA) and Tristan Dagobert (CMLA)',
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='>=3.5',
+    zip_safe=False
+)
