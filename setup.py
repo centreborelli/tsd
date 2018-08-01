3009102b48508e136686afee2d2da189910ff93b
Nicolas Conti <n.conti@kayrros.com>
2018-08-01 13:40:02 +0200

    Trying to fix pip install on docker image

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup, find_packages
-import re
-import ast
-
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('tsd/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
-
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -15,7 +7,6 @@ setup(
     name="tsd",
     version="0.1",
     description='Automatic download of Sentinel, Landsat and Planet crops.',
-    long_description=readme(),
     author='Carlo de Franchis (CMLA)'
            'With contributions from Enric Meinhardt-Llopis (ENS Cachan), Axel Davy (CMLA) and Tristan Dagobert (CMLA)',
     packages=find_packages(),
