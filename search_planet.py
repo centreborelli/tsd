f7b7bcfc12b5e51e6a27bdc698ff4e491f42a975
Carlo de Franchis <carlodef@gmail.com>
2018-08-23 21:02:47 +0200

    [doc] add license information

--- search_planet.py
+++ search_planet.py
@@ -5,7 +5,20 @@
 """
 Search of Planet images.
 
-Copyright (C) 2016-17, Carlo de Franchis <carlo.de-franchis@m4x.org>
+Copyright (C) 2016-18, Carlo de Franchis <carlo.de-franchis@m4x.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from __future__ import print_function
