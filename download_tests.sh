b5c286a88db13d0bac53c7d525760d64c4e4f440
Carlo de Franchis <carlodef@gmail.com>
2019-01-14 17:35:15 +0100

    [tests] update download tests

--- download_tests.sh
+++ download_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir -p tests
-gdal_translate --config GDAL_DISABLE_READDIR_ON_OPEN YES --config CPL_VSIL_CURL_ALLOWED_EXTENSIONS TIF /vsicurl/http://landsat-pds.s3.amazonaws.com/L8/030/038/LC80300382015126LGN00/LC80300382015126LGN00_B8.TIF tests/l8.tif -of GTiff -srcwin 2400 3500 500 500
-gdal_translate --config GDAL_DISABLE_READDIR_ON_OPEN YES --config CPL_VSIL_CURL_ALLOWED_EXTENSIONS jp2 --config AWS_REQUEST_PAYER requester /vsis3/sentinel-s2-l1c/tiles/13/S/GR/2016/5/11/0/B04.jp2 tests/s2.tif -of GTiff -ot UInt16 -srcwin 2400 3500 500 500
-python3 tsd/get_landsat.py --lon -102.5364 --lat 32.4396 -w 5000 -l 5000 -s 2015-04-05 -e 2015-07-20 -o tests/l8
-python3 tsd/get_sentinel2.py --lon -102.5364 --lat 32.4396 -w 5000 -l 5000 -s 2016-04-05 -e 2016-07-20 -o tests/s2
+gdal_translate --config GDAL_DISABLE_READDIR_ON_OPEN YES --config CPL_VSIL_CURL_ALLOWED_EXTENSIONS TIF /vsicurl/http://landsat-pds.s3.amazonaws.com/c1/L8/176/039/LC08_L1TP_176039_20180614_20180703_01_T1/LC08_L1TP_176039_20180614_20180703_01_T1_B8.TIF tests/l8.tif -of GTiff -projwin 317533 3320455 322533 3315455 -projwin_srs "+proj=utm +zone=36"
+gdal_translate --config GDAL_DISABLE_READDIR_ON_OPEN YES --config CPL_VSIL_CURL_ALLOWED_EXTENSIONS jp2 /vsicurl/http://storage.googleapis.com/gcp-public-data-sentinel-2/tiles/36/R/UU/S2A_MSIL1C_20180728T082601_N0206_R021_T36RUU_20180728T104559.SAFE/GRANULE/L1C_T36RUU_A016177_20180728T084109/IMG_DATA/T36RUU_20180728T082601_B04.jp2 tests/s2.tif -of GTiff -ot UInt16 -projwin 317520 3320460 322560 3315480 -projwin_srs "+proj=utm +zone=36"
+python3 tsd/get_landsat.py --lon 31.1346 --lat 29.9793 -w 5000 -l 5000 -s 2015-04-05 -e 2015-07-20 -o tests/l8
+python3 tsd/get_sentinel2.py --lon 31.1346 --lat 29.9793 -w 5000 -l 5000 -s 2016-04-05 -e 2016-05-12 -o tests/s2
 
 # test the 8 (api, mirror) combinations
 #for API in devseed planet scihub gcloud
