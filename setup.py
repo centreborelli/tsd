02fd884c80ab798f15f3dc059589339231907c67
Carlo de Franchis <carlodef@gmail.com>
2019-04-20 10:52:34 +0200

    [build] fix packaging for upload on PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-from setuptools import setup, find_packages
+import os
+from codecs import open
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+package = "tsd"
+
+about = {}
+with open(os.path.join(here, package, "__about__.py"), "r", "utf-8") as f:
+    exec(f.read(), about)
+
+def readme():
+    with open(os.path.join(here, 'README.md'), 'r', 'utf-8') as f:
+        return f.read()
 
 requirements = ['area',
                 'boto3',
@@ -21,14 +35,17 @@ requirements = ['area',
                 'tqdm',
                 'utm']
 
-setup(name="tsd",
-      version="0.2",
-      description='Automatic download of Sentinel, Landsat and Planet crops.',
-      author='Carlo de Franchis (CMLA)'
-             'With contributions from Enric Meinhardt-Llopis (ENS Cachan), Axel Davy (CMLA) and Tristan Dagobert (CMLA)',
-      packages=find_packages(),
-      install_requires=requirements,
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__description__"],
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url=about["__url__"],
+      author=about["__author__"],
+      author_email=about["__author_email__"],
+      packages=[package],
       package_data={'': ['s2_mgrs_grid.txt']},
       include_package_data=True,
-      python_requires='>=3.5'
-)
+      install_requires=requirements,
+      python_requires=">=3.5")
