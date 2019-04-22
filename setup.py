14ccae44a6f5432208d6e568070e8e76158dfdd0
Gabriele Facciolo <gfacciol@gmail.com>
2019-04-21 23:28:26 -0300

    Finishing replacing mkdir_p with os.makedirs(..., exist_ok=True)

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ requirements = ['area',
                 'sat-search>=0.2',
                 'shapely',
                 'tqdm',
-                'utm']
+                'utm',
+                'sentinelhub']
 
 setup(name="tsd",
       version="0.2",
