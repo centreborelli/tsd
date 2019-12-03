centreborelli/tsd

commit 2036c26edd8f5fe1051cecd7af419bf34d19ba65

[dev] fix pyproj warning

tsd/utils.py
@@ -327,8 +327,8 @@ def pyproj_transform(x, y, in_epsg, out_epsg):
         scalar or array: x coordinate(s) of the point(s) in the output EPSG system
         scalar or array: y coordinate(s) of the point(s) in the output EPSG system
     """
-    in_proj = pyproj.Proj(init="epsg:{}".format(in_epsg))
-    out_proj = pyproj.Proj(init="epsg:{}".format(out_epsg))
+    in_proj = pyproj.Proj("epsg:{}".format(in_epsg))
+    out_proj = pyproj.Proj("epsg:{}".format(out_epsg))
     return pyproj.transform(in_proj, out_proj, x, y)
 
 

commit 1b1e3bd74dc110657097292c1c1680c4dac3d2a6

[bugfix] fix bug introduced in commit 2036c26edd trying to fix a warning

tsd/utils.py
@@ -327,8 +327,8 @@ def pyproj_transform(x, y, in_epsg, out_epsg):
         scalar or array: x coordinate(s) of the point(s) in the output EPSG system
         scalar or array: y coordinate(s) of the point(s) in the output EPSG system
     """
-    in_proj = pyproj.Proj("epsg:{}".format(in_epsg))
-    out_proj = pyproj.Proj("epsg:{}".format(out_epsg))
+    in_proj = pyproj.Proj(init="epsg:{}".format(in_epsg))
+    out_proj = pyproj.Proj(init="epsg:{}".format(out_epsg))
     return pyproj.transform(in_proj, out_proj, x, y)
 
 