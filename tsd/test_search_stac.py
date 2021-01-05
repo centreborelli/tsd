d079740b7b37a1d20cb04eb63dc8a88cc4ce2896
Carlo de Franchis <carlodef@gmail.com>
2021-01-05 18:43:14 +0100

    [tests] update test_search_stac.py for Sentinel-2 new default product_type

--- tsd/test_search_stac.py
+++ tsd/test_search_stac.py
@@ -7,9 +7,10 @@ aoi = utils.geojson_geometry_object(29.9793, 31.1346, 5000, 5000)
 results = search_stac.search(aoi,
                              start_date=datetime.datetime(2019, 1, 15),
                              end_date=datetime.datetime(2019, 1, 30),
-                             satellite='Sentinel-2')
-
-expected_titles = ['S2B_MSIL1C_20190129T083209_N0207_R021_T36RUU_20190129T103220',
-                   'S2A_MSIL1C_20190124T083231_N0207_R021_T36RUU_20190124T095836',
-                   'S2B_MSIL1C_20190119T083259_N0207_R021_T36RUU_20190119T104924']
-assert([r['properties']['sentinel:product_id'] for r in results] == expected_titles)
+                             satellite='Sentinel-2',
+                             product_type="L1C")
+computed = [r['properties']['sentinel:product_id'] for r in results]
+expected = ['S2B_MSIL1C_20190129T083209_N0207_R021_T36RUU_20190129T103220',
+            'S2A_MSIL1C_20190124T083231_N0207_R021_T36RUU_20190124T095836',
+            'S2B_MSIL1C_20190119T083259_N0207_R021_T36RUU_20190119T104924']
+assert computed == expected, computed
