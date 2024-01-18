b52cfcef4161cb3a91a5256a122fb04b3454e73f
Carlo de Franchis <carlodef@gmail.com>
2024-01-18 22:31:38 +0100

    update version number to 0.9.0

--- tsd/__about__.py
+++ tsd/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "tsd"
 __description__ = "Search and download of Sentinel, Landsat and Planet crops."
-__url__ = 'https://github.com/cmla/tsd'
-__version__ = "0.8.1"
+__url__ = 'https://github.com/centreborelli/tsd'
+__version__ = "0.9.0"
 __author__ = "Carlo de Franchis"
 __author_email__ = 'carlo.de-franchis@ens-paris-saclay.fr'
